90832b8727372891a11c78a906104b1807cb818b
ossygarcia <mrbrucekessy@gmail.com>
2022-01-28 00:11:58 +0100

    Netflix_app

--- API/server.js
+++ API/server.js
@@ -2,6 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const dotenv = require('dotenv')
+const cors = require('cors');
+
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 const Port = process.env.PORT || 5000
 
 //Imports
